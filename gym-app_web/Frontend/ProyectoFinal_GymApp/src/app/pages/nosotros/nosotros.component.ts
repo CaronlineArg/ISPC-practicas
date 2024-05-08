98c6b602911fe814f29a47ff4b3356ff51853ea6
Sole1988 <sole.mattos2@gmail.com>
2024-05-08 20:27:50 -0300

    estilos scss

--- gym-app_web/Frontend/ProyectoFinal_GymApp/src/app/pages/nosotros/nosotros.component.ts
+++ gym-app_web/Frontend/ProyectoFinal_GymApp/src/app/pages/nosotros/nosotros.component.ts
@@ -3,14 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nosotros',
   templateUrl: './nosotros.component.html',
-  styleUrls: ['./nosotros.component.css']
+  styleUrls: ['./nosotros.component.scss']
 })
 export class NosotrosComponent {
 
   colorFondo: string = '#ffffff'; // Asigna el color de fondo blanco
   colorTexto: string = '#000000'; // Asigna el color de texto negro
+  colorTextoBoton: string = '#ffffff';
   colorBotones: string = '#dc3545'; // Asigna el color del botón (rojo)
-  tamanioBoton: string = '24px'; // Asigna el tamaño del botón
+  tamanioBoton: string = '30px'; // Asigna el tamaño del botón
   radioBordesBoton: string = '5px'; // Asigna el radio de los bordes del botón
   tamanoImagen: string = '250px';
 }
