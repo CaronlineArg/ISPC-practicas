2f7535688d4aa2e04058dccb21420d0509568b2f
Sole1988 <sole.mattos2@gmail.com>
2024-05-08 19:36:15 -0300

    estilos scss

--- gym-app_web/Frontend/ProyectoFinal_GymApp/src/app/pages/nosotros/nosotros.component.ts
+++ gym-app_web/Frontend/ProyectoFinal_GymApp/src/app/pages/nosotros/nosotros.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class NosotrosComponent {
 
+  colorFondo: string = '#ffffff'; // Asigna el color de fondo blanco
+  colorTexto: string = '#000000'; // Asigna el color de texto negro
+  colorBotones: string = '#dc3545'; // Asigna el color del botón (rojo)
+  tamanioBoton: string = '24px'; // Asigna el tamaño del botón
+  radioBordesBoton: string = '5px'; // Asigna el radio de los bordes del botón
+  tamanoImagen: string = '250px';
 }
+
