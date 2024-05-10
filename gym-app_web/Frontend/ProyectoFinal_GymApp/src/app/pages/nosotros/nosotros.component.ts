348d04dbd8e0875ff4de0b32190c3e30c04f10db
Sole1988 <sole.mattos2@gmail.com>
2024-05-10 20:11:21 -0300

    estilos imagenes

--- gym-app_web/Frontend/ProyectoFinal_GymApp/src/app/pages/nosotros/nosotros.component.ts
+++ gym-app_web/Frontend/ProyectoFinal_GymApp/src/app/pages/nosotros/nosotros.component.ts
@@ -7,10 +7,11 @@ import { Component } from '@angular/core';
 })
 export class NosotrosComponent {
 
-  colorFondo: string = '#ffffff'; // Asigna el color de fondo blanco
-  colorTexto: string = '#000000'; // Asigna el color de texto negro
+  colorFondo: string = '#ffffff'; // color de fondo blanco
+  colorTexto: string = '#000000'; //  color de texto negro
+  colorSeccion: string = '#8e9092'; //color gris
   colorTextoBoton: string = '#ffffff';
-  colorBotones: string = '#dc3545'; // Asigna el color del botón (rojo)
+  colorBotones: string = '#dc3545'; //  color del botón (rojo)
   tamanioBoton: string = '30px'; // Asigna el tamaño del botón
   radioBordesBoton: string = '5px'; // Asigna el radio de los bordes del botón
   tamanoImagen: string = '250px';
